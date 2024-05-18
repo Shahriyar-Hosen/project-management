a324383ca669d7a002b26b9070865f8dd73cfb74
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-05-18 14:06:32 +0600

    clean up

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,27 +1,19 @@
 "use client";
 
 import { ProjectHead } from "@/components/projects";
+import { useCurrentUser } from "@/hooks/use-auth";
 import { getAllProject } from "@/server/actions";
-import { useStores } from "@/stores/provider";
 import { NextPage } from "next";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 const Home: NextPage = () => {
-  const { user, logout, count, incrementCount, decrementCount } = useStores(
-    (state) => state
-  );
+  const user = useCurrentUser();
   const router = useRouter();
 
   useEffect(() => {
     !user && router.push("/login");
   }, [router, user]);
-
-  const logoutFn = () => {
-    localStorage.removeItem("user");
-    logout();
-    router.push("/login");
-  };
 
   useEffect(() => {
     const projects = async () => {
@@ -34,18 +26,6 @@ const Home: NextPage = () => {
   return (
     <main>
       <ProjectHead />
-      <div>
-        Count: {count}
-        <hr />
-        <button type="button" onClick={() => incrementCount()}>
-          Increment Count
-        </button>
-        <button type="button" onClick={() => decrementCount()}>
-          Decrement Count
-        </button>
-      </div>
-      <button onClick={logoutFn}>logout</button>
-      <h1>Home</h1>
     </main>
   );
 };
